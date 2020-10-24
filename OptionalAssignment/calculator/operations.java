8a252cf4478bea6ee052ab889a5c2faabe98fe9c
Siriusssssssssssssssssssss <f20190041@goa.bits-pilani.ac.in>
2020-10-24 13:38:08 +0530

    added

---
+++ OptionalAssignment/calculator/operations.java
@@ -0,0 +1,16 @@
+package calculator;
+
+public class operations {
+    public static float sum(float a,float b){
+        return a+b;
+    }
+
+    public static float multiplication(float a,float b){
+        return a*b;
+    }
+    public static float division(float a,float b){
+        return a/b;
+    }
+
+}
+
